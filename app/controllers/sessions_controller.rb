f436df16c44fe839366b61c99c20eaf39aeeae18
Elle Faraday <faraday.elle@gmail.com>
2018-08-02 18:56:59 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     if params[:name] && params[:name] != ""
-      session[:name]
+      session[:name] = params[:name]
       redirect_to '/'
     else
       redirect_to "/login"
